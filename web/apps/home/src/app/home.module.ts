73a723dee54c7bfadbe597af1dfc57bd156f9006
Janik Schumacher <rndmb0t@gmail.com>
2022-03-08 18:43:29 +0100

    moved

--- web/apps/home/src/app/home.module.ts
+++ web/apps/home/src/app/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
