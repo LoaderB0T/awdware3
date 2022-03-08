36c5fd19865082ab755db04996bc8a2c86c10849
Janik Schumacher <rndmb0t@gmail.com>
2022-03-08 22:23:14 +0100

    home component WIP

--- web/apps/home/src/app/home/home.component.ts
+++ web/apps/home/src/app/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Typing } from './typing';
 
 @Component({
   selector: 'awd-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit {
+  public readonly typing = new Typing();
+
+  public ngOnInit(): void {
+    this.typing.start('DEVELOPER DEVELOPER DEVELOPER');
+  }
 }
--- web/apps/host/src/app/app.module.ts
+++ web/apps/host/src/app/app.module.ts
@@ -3,20 +3,32 @@ import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ThemeService } from '@awdware/shared';
+import { SharedModule, ThemeService } from '@awdware/shared';
 import { BaseComponent } from './base/base.component';
 import { BgComponent } from './bg/bg.component';
 import { routes } from './routes';
-import { loadedModules } from 'ng-dynamic-mf';
+import { DynamicTranslationService, loadedModules } from 'ng-dynamic-mf';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AppComponent, BaseComponent, BgComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }), loadedModules],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+    SharedModule,
+    TranslateModule.forRoot({}),
+    loadedModules
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(themeService: ThemeService) {
+  constructor(
+    themeService: ThemeService,
+    translateService: TranslateService,
+    dynamicTranslationService: DynamicTranslationService
+  ) {
     themeService.init();
+    dynamicTranslationService.setTranslateService(translateService);
   }
 }
