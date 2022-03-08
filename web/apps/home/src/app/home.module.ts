6f4417c7fd8c9faff55abe7bc95521af23f60fe7
Janik Schumacher <rndmb0t@gmail.com>
2022-03-08 22:22:46 +0100

    translations in home

--- web/apps/home/src/app/home.module.ts
+++ web/apps/home/src/app/home.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TranslateService } from '@ngx-translate/core';
+import { DynamicTranslationService } from 'ng-dynamic-mf';
+import { SharedModule } from '@awdware/shared';
 
 const routes: Routes = [
   {
@@ -12,6 +15,11 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)]
 })
-export class HomeModule {}
+export class HomeModule {
+  constructor(translateService: TranslateService, dynamicTranslationService: DynamicTranslationService) {
+    dynamicTranslationService.setTranslateService(translateService);
+    dynamicTranslationService.registerTranslations(['de', 'en'], l => `assets/locales/${l}.json`, 'home');
+  }
+}
