a4c096413bd3cf468c076b2d6cd24eafe1778722
Janik Schumacher <rndmb0t@gmail.com>
2022-03-09 14:28:32 +0100

    adjusted entry animations

--- web/apps/host/src/app/base/router-animation.ts
+++ web/apps/host/src/app/base/router-animation.ts
@@ -30,8 +30,11 @@ const fadeIn = [
     query(
       ':enter',
       [
-        style({ transform: 'translateX(-300px)', opacity: '0.2' }),
-        animate('300ms cubic-bezier(0,.33,0,1)', style({ transform: 'translateX(0)', opacity: '1' }))
+        style({ transform: 'translateX(0px) scale(1.2)', opacity: '0' }),
+        group([
+          animate('500ms 400ms cubic-bezier(0,.5,.2,1)', style({ transform: 'translateX(0) scale(1)' })),
+          animate('1s 400ms cubic-bezier(0,.5,.2,1)', style({ opacity: '1' }))
+        ])
       ],
       { optional: true }
     )
