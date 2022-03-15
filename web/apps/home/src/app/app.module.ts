37689e1677a3af275250e5d4daebe93c0c30a48d
Janik Schumacher <rndmb0t@gmail.com>
2022-03-15 16:06:58 +0100

    home standalone fixed

--- web/apps/home/src/app/app.module.ts
+++ web/apps/home/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 // Required for TS to compile the "unused" RemoteEntryModule
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { RemoteEntryModule } from './home-remote.module';
-import { HomeModule } from './home.module';
+import { SharedModule, ThemeService, TranslationService } from '@awdware/shared';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HomeModule, RouterModule.forRoot([])],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        loadChildren: () => import('./home.module').then(m => m.HomeModule)
+      }
+    ]),
+    SharedModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({})
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(themeService: ThemeService, translationService: TranslationService) {
+    themeService.init();
+    translationService.init();
+  }
+}
