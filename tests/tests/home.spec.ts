b3b46b8b8b444a041f0d999493572c1a376af495
Janik Schumacher <rndmb0t@gmail.com>
2022-03-25 19:39:19 +0100

    .

--- tests/tests/home.spec.ts
+++ tests/tests/home.spec.ts
@@ -6,7 +6,6 @@ test.beforeEach(async ({ page, baseURL }) => {
 
 test.describe("Home Module", () => {
   test("should navigate to home", async ({ page, baseURL }) => {
-    debugger;
     await page.waitForURL(baseURL + "/home");
   });
 });
