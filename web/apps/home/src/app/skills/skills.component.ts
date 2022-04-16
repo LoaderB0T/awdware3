a0417b8823abc43b373f4240bbcd1418226fcd68
Janik Schumacher <rndmb0t@gmail.com>
2022-04-16 21:09:39 +0200

    hmm

--- web/apps/home/src/app/skills/skills.component.ts
+++ web/apps/home/src/app/skills/skills.component.ts
@@ -12,13 +12,31 @@ import { BehaviorSubject } from 'rxjs';
 export class SkillsComponent {
   public skills = skills;
   public readonly knowledge$ = new BehaviorSubject<Knowledge[]>([]);
+  private readonly _intervals: Readonly<number[]> = [20, 40, 60, 80, 100, 110, 120, 130, 140, 150] as const;
+  private _currentInterval: number = 0;
 
   constructor() {
-    this.getNextRandomKnowledge();
+    this.setRandomKnowledge();
   }
 
-  public getNextRandomKnowledge() {
+  public startRandomKnowledgeInterval() {
+    this.setRandomKnowledge();
+    this._currentInterval = 0;
+    this.nextRandomKnowledgeInterval();
+  }
+
+  private setRandomKnowledge() {
     knowledge.sort(() => 0.5 - Math.random());
     this.knowledge$.next(knowledge.slice(0, 5));
+  }
+
+  private nextRandomKnowledgeInterval() {
+    setTimeout(() => {
+      this.setRandomKnowledge();
+      if (this._currentInterval < this._intervals.length - 1) {
+        this._currentInterval++;
+        this.nextRandomKnowledgeInterval();
+      }
+    }, this._intervals[this._currentInterval]);
   }
 }
