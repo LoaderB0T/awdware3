01df86c0893cd3e72d2e52b477686001113d25a0
Janik Schumacher <rndmb0t@gmail.com>
2022-04-17 17:03:55 +0200

    cd strategy

--- web/apps/projects/src/app/projects-base/projects-base.component.ts
+++ web/apps/projects/src/app/projects-base/projects-base.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { projects } from './projects';
 
 @Component({
   templateUrl: './projects-base.component.html',
-  styleUrls: ['./projects-base.component.scss']
+  styleUrls: ['./projects-base.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProjectsBaseComponent {
   public projects = projects;
