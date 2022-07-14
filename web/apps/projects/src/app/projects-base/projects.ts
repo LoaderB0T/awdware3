45ba07b824df08e0edb6bbfea327f0c61ec1488a
Janik Schumacher <rndmb0t@gmail.com>
2022-07-14 23:13:03 +0200

    confetti.ts project

--- web/apps/projects/src/app/projects-base/projects.ts
+++ web/apps/projects/src/app/projects-base/projects.ts
@@ -24,7 +24,6 @@ export const projects: Project[] = [
         'awesome-logging is a logging and prompting library for CLI applications written in NodeJS. ' +
         'It is written in TypeScript and provides a strongly-typed, simple, yet powerful API.'
     },
-
     gitHubUser: 'LoaderB0T'
   },
   {
@@ -34,7 +33,6 @@ export const projects: Project[] = [
         'ng-dynamic-mf is a helper library for Module Federation in Angular. ' +
         'It makes loading modules fully dynamic at runtime, helps with asset resolution, translations and routing across apps.'
     },
-
     gitHubUser: 'LoaderB0T'
   },
   {
@@ -44,7 +42,6 @@ export const projects: Project[] = [
         'paint-increment is a fun project to collaboatively and iteratively paint on a shared canvas. ' +
         'The idea is to create a shirt with friends that has the final painting and all iterations printed on it.'
     },
-
     gitHubUser: 'LoaderB0T'
   },
   {
@@ -54,7 +51,6 @@ export const projects: Project[] = [
         'typed.ts is a small library for creating realistic typing animations in TypeScript or JavaScript. ' +
         'The library is configurable in many ways and will randomly type errors and correct them automatically.'
     },
-
     gitHubUser: 'LoaderB0T'
   },
   {
@@ -64,7 +60,15 @@ export const projects: Project[] = [
         'easy-network-stub provides an easy-to-use and type-safe network stub API for NodeJS based e2e testing frameworks. ' +
         'It is meant for test scenarios where you want to mock each and every request to a remote API.'
     },
-
+    gitHubUser: 'LoaderB0T'
+  },
+  {
+    name: 'confetti.ts',
+    descriptions: {
+      en:
+        'confetti.ts is a library for creating particle animations in TypeScript or JavaScript in a HTML canvas. ' +
+        'It is lightweight, has a simple API and is extensible with custom particle types.'
+    },
     gitHubUser: 'LoaderB0T'
   }
 ];
