d4bdb9c1fefd7c3f0706e7de9da5941db61fecc3
Janik Schumacher <rndmb0t@gmail.com>
2022-08-18 10:44:15 +0200

    nx migrate

--- web/libs/controls/jest.config.ts
+++ web/libs/controls/jest.config.ts
@@ -1,21 +1,22 @@
+/* eslint-disable */
 module.exports = {
-  displayName: 'host',
+  displayName: 'controls',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
+      stringifyContentPathRegex: '\\.(html|svg)$'
+    }
   },
-  coverageDirectory: '../../coverage/apps/host',
+  coverageDirectory: '../../coverage/libs/controls',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
+    'jest-preset-angular/build/serializers/html-comment'
+  ]
 };
