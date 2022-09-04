c63266ec23ca7ec69b9f4fde5645492d8c16a7b8
Janik Schumacher <rndmb0t@gmail.com>
2022-09-04 22:30:31 +0200

    skills rework

--- web/apps/home/src/app/skills/skills.component.ts
+++ web/apps/home/src/app/skills/skills.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { skills } from './skills';
-import { Knowledge, knowledge } from './knowledge';
-import { BehaviorSubject } from 'rxjs';
+import { knowledge } from './knowledge';
 
 @Component({
   selector: 'awd-skills',
@@ -11,32 +10,5 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class SkillsComponent {
   public skills = skills;
-  public readonly knowledge$ = new BehaviorSubject<Knowledge[]>([]);
-  private readonly _intervals: Readonly<number[]> = [20, 40, 60, 80, 100, 110, 120, 130, 140, 150] as const;
-  private _currentInterval: number = 0;
-
-  constructor() {
-    this.setRandomKnowledge();
-  }
-
-  public startRandomKnowledgeInterval() {
-    this.setRandomKnowledge();
-    this._currentInterval = 0;
-    this.nextRandomKnowledgeInterval();
-  }
-
-  private setRandomKnowledge() {
-    knowledge.sort(() => 0.5 - Math.random());
-    this.knowledge$.next(knowledge.slice(0, 5));
-  }
-
-  private nextRandomKnowledgeInterval() {
-    setTimeout(() => {
-      this.setRandomKnowledge();
-      if (this._currentInterval < this._intervals.length - 1) {
-        this._currentInterval++;
-        this.nextRandomKnowledgeInterval();
-      }
-    }, this._intervals[this._currentInterval]);
-  }
+  public knowledge = knowledge.sort(() => Math.random() - 0.5);
 }
