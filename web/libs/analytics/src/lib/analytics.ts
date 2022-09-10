cae0ebb0706c07310fd6cc14ec90b8aa3868e1d9
Janik Schumacher <rndmb0t@gmail.com>
2022-09-10 02:52:36 +0200

    analytics toggle

--- web/libs/analytics/src/lib/analytics.ts
+++ web/libs/analytics/src/lib/analytics.ts
@@ -3,14 +3,19 @@ type Umami = {
   trackView: (name: string, referrer?: string, website_id?: string) => void;
 };
 
-const umamiWindow = window as typeof window & { umami?: Umami };
-
 type AwdwareAnalytics = {
   trackEvent: Umami['trackEvent'];
 };
 
+const umamiWindow = window as typeof window & { umami?: Umami };
+
+const disableAnalytics = localStorage.getItem('disableAnalytics') === 'true';
+
 export const analytics: AwdwareAnalytics = {
   trackEvent: (name: string, data?: any) => {
+    if (disableAnalytics) {
+      return;
+    }
     try {
       umamiWindow.umami?.trackEvent(name, data);
     } catch (error) {
