a815fa92924cc38ccc0158ab302e32f822722c62
Janik Schumacher <rndmb0t@gmail.com>
2022-10-06 23:26:28 +0200

    idk

--- web/apps/timeline/src/app/timeline-base/timeline/timeline.ts
+++ web/apps/timeline/src/app/timeline-base/timeline/timeline.ts
@@ -3,6 +3,7 @@ export type TimelineEntry = {
   image?: string;
   from: number;
   to: number | 'today';
+  name: string;
 };
 
 export type TimelineViewModel = {
@@ -14,26 +15,31 @@ export type TimelineEntryViewModel = {
   image?: string;
   from: number;
   to: number;
+  name: string;
 };
 
 export const timelime: TimelineEntry[] = [
   {
     id: 'awdware0',
+    name: '1.0',
     from: 2014,
     to: 2016
   },
   {
     id: 'awdware1',
+    name: '1.1',
     from: 2016,
     to: 2019
   },
   {
     id: 'awdware2',
+    name: '2.0',
     from: 2019,
     to: 2022
   },
   {
     id: 'awdware3',
+    name: '3.0',
     from: 2022,
     to: 'today'
   }
