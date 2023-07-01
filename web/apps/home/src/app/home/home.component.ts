b9d8198e4ba1c4bfc8d702dd9bf70f41824c5538
Janik Schumacher <rndmb0t@gmail.com>
2023-07-02 00:29:43 +0200

    signals 🎉

--- web/apps/home/src/app/home/home.component.ts
+++ web/apps/home/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+
 import { Router } from '@angular/router';
 import { PreloadService, TranslationService } from '@awdware/shared';
 import { analytics } from '@awdware/analytics';
@@ -16,15 +18,19 @@ export class HomeComponent implements OnInit {
   private readonly _translateService: TranslateService;
   private readonly _translationService: TranslationService;
   private readonly _router: Router;
-  private readonly _langChanged = new BehaviorSubject(false);
-  public readonly langChanged$ = this._langChanged.asObservable();
+  public readonly langChanged = signal(false);
 
-  public readonly typing1 = new Typed({ noSpecialCharErrors: true });
-  public readonly typing2 = new Typed({ noSpecialCharErrors: true });
-  public readonly typing3 = new Typed({ noSpecialCharErrors: true });
+  private readonly _typing1 = new Typed({ noSpecialCharErrors: true });
+  private readonly _typing2 = new Typed({ noSpecialCharErrors: true });
+  private readonly _typing3 = new Typed({ noSpecialCharErrors: true });
+
+  public readonly typing1 = toSignal(this._typing1.text$);
+  public readonly typing2 = toSignal(this._typing2.text$);
+  public readonly typing3 = toSignal(this._typing3.text$);
+
   public skip = false;
   public done$ = new BehaviorSubject(false);
-  public readonly fillerLines$ = this.typing1.text$.pipe(
+  public readonly fillerLines$ = this._typing1.text$.pipe(
     map(t => {
       return new Array(3 - t.split('\n').length);
     })
@@ -46,7 +52,7 @@ export class HomeComponent implements OnInit {
     const click = this._translateService.instant('home.typing.click');
     const here = this._translateService.instant('home.typing.here');
     const toLearnMore = this._translateService.instant('home.typing.toLearnMore');
-    this.typing1
+    this._typing1
       .wait(1500)
       .type(hi)
       .wait(100)
@@ -59,15 +65,15 @@ export class HomeComponent implements OnInit {
       .type('\n')
       .wait(600);
     this.typeLikes();
-    this.typing1.type(`${click} `);
-    this.typing2.type(here);
-    this.typing3.type(` ${toLearnMore}`);
+    this._typing1.type(`${click} `);
+    this._typing2.type(here);
+    this._typing3.type(` ${toLearnMore}`);
   }
 
   public ngOnInit(): void {
     this.typeIntro();
     this._translationService.languageChanged$.subscribe(() => {
-      this._langChanged.next(true);
+      this.langChanged.set(true);
       if (!this.skip) {
         this.fastForward();
       }
@@ -76,9 +82,9 @@ export class HomeComponent implements OnInit {
 
   private async typeIntro(): Promise<void> {
     this.done$.next(false);
-    await this.typing1.run();
-    await this.typing2.run();
-    await this.typing3.run();
+    await this._typing1.run();
+    await this._typing2.run();
+    await this._typing3.run();
     this.done$.next(true);
   }
 
@@ -86,33 +92,33 @@ export class HomeComponent implements OnInit {
     const ilike = this._translateService.instant('home.typing.ilike');
 
     if (!this.skip) {
-      this.typing1.type(`${ilike} `);
+      this._typing1.type(`${ilike} `);
       const likes = ['Web Development', 'TypeScript', 'Angular', 'C# & .NET', 'Automation (CI/CD)'];
       for (const like of likes) {
-        this.typing1
+        this._typing1
           .wait(100)
           .type(like)
           .wait(500)
           .backspace(like.length, { eraseDelay: { min: 30, max: 60 } });
       }
-      this.typing1.backspace(ilike.length + 1, { eraseDelay: { min: 30, max: 60 } });
+      this._typing1.backspace(ilike.length + 1, { eraseDelay: { min: 30, max: 60 } });
     }
   }
 
   public async restart() {
     analytics.track('home.restart');
     this.skip = false;
-    await Promise.all([this.typing1.reset(), this.typing2.reset(), this.typing3.reset()]);
-    this._langChanged.next(false);
+    await Promise.all([this._typing1.reset(), this._typing2.reset(), this._typing3.reset()]);
+    this.langChanged.set(false);
     this.typeIntro();
   }
 
   public fastForward() {
     analytics.track('home.fastForward');
     this.skip = true;
-    this.typing1.fastForward();
-    this.typing2.fastForward();
-    this.typing3.fastForward();
+    this._typing1.fastForward();
+    this._typing2.fastForward();
+    this._typing3.fastForward();
   }
 
   public learnMore() {
