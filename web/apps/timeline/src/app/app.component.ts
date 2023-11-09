6e286a43f6a600d99498bb0250811477b8dd8389
Janik Schumacher <rndmb0t@gmail.com>
2023-11-10 00:37:15 +0100

    self closing tags

--- web/apps/timeline/src/app/app.component.ts
+++ web/apps/timeline/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'awd-root',
-  template: '<router-outlet></router-outlet>',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  template: '<router-outlet />',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
