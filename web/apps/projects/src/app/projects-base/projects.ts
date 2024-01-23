c5437274564ac87b6c80bb465673df59ebf1c9f2
Janik Schumacher <rndmb0t@gmail.com>
2024-01-24 00:36:55 +0100

    fix

--- web/apps/projects/src/app/projects-base/projects.ts
+++ web/apps/projects/src/app/projects-base/projects.ts
@@ -1,7 +1,7 @@
 export type Project = {
   name: string;
   descriptions: {
-    [lang: string]: string;
+    [lang: string]: string | undefined;
   };
   gitHubUser: string;
 };
@@ -17,9 +17,9 @@ export const projects: Project[] = [
       de:
         'awdware3 ist die aktuelle Version dieser Website. ' +
         'Es ist die dritte Iteration der awdware Website, ' +
-        'welche mit Angular und Module Federation gebaut ist.'
+        'welche mit Angular und Module Federation gebaut ist.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'awesome-logging',
@@ -29,9 +29,9 @@ export const projects: Project[] = [
         'It is written in TypeScript and provides a strongly-typed, simple, yet powerful API.',
       de:
         'awesome-logging ist eine Logging- und Prompting-Library für CLI-Anwendungen, welche in NodeJS geschrieben sind. ' +
-        'Es ist in TypeScript geschrieben und bietet eine strikt typisierte, einfache, aber leistungsstarke API.'
+        'Es ist in TypeScript geschrieben und bietet eine strikt typisierte, einfache, aber leistungsstarke API.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'ng-dynamic-mf',
@@ -41,9 +41,9 @@ export const projects: Project[] = [
         'It makes loading modules fully dynamic at runtime, helps with asset resolution, translations and routing across apps.',
       de:
         'ng-dynamic-mf ist eine Hilfsbibliothek für Module Federation in Angular. ' +
-        'Es ermöglicht das dynamische Laden von Modulen zur Laufzeit, hilft bei der Auflösung von Assets, Übersetzungen und Routing zwischen Apps.'
+        'Es ermöglicht das dynamische Laden von Modulen zur Laufzeit, hilft bei der Auflösung von Assets, Übersetzungen und Routing zwischen Apps.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'paint-increment',
@@ -53,9 +53,9 @@ export const projects: Project[] = [
         'The idea is to create a shirt with friends that has the final painting and all iterations printed on it.',
       de:
         'paint-increment ist ein Spaßprojekt, um gemeinsam und iterativ auf einer gemeinsamen Leinwand zu malen. ' +
-        'Die Idee ist es, ein T-Shirt mit Freunden zu erstellen, welches das fertige Bild und alle Iterationen darauf gedruckt hat.'
+        'Die Idee ist es, ein T-Shirt mit Freunden zu erstellen, welches das fertige Bild und alle Iterationen darauf gedruckt hat.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'typed.ts',
@@ -65,9 +65,9 @@ export const projects: Project[] = [
         'The library is configurable in many ways and will randomly type errors and correct them automatically.',
       de:
         'typed.ts ist eine kleine Bibliothek für die Erstellung realistischer Tipp-Animationen in TypeScript oder JavaScript. ' +
-        'Die Bibliothek ist in vielen Bereichen konfigurierbar und wird zufällig Fehler tippen und diese automatisch korrigieren.'
+        'Die Bibliothek ist in vielen Bereichen konfigurierbar und wird zufällig Fehler tippen und diese automatisch korrigieren.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'easy-network-stub',
@@ -77,9 +77,9 @@ export const projects: Project[] = [
         'It is meant for test scenarios where you want to mock each and every request to a remote API.',
       de:
         'easy-network-stub bietet eine einfach zu bedienende und typsichere Netzwerk-Stub-API für NodeJS-basierte E2E-Testframeworks. ' +
-        'Es ist für Testszenarien gedacht, bei denen Sie jede einzelne Anfrage an eine Remote-API simulieren möchten.'
+        'Es ist für Testszenarien gedacht, bei denen Sie jede einzelne Anfrage an eine Remote-API simulieren möchten.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'confetti.ts',
@@ -89,9 +89,9 @@ export const projects: Project[] = [
         'It is lightweight, has a simple API and is extensible with custom particle types.',
       de:
         'confetti.ts ist eine Bibliothek für die Erstellung von Partikel-Animationen in TypeScript oder JavaScript in einem HTML Canvas. ' +
-        'Die Bibliothek ist leichtgewichtig, hat eine einfache API und ist erweiterbar mit benutzerdefinierten Partikeltypen.'
+        'Die Bibliothek ist leichtgewichtig, hat eine einfache API und ist erweiterbar mit benutzerdefinierten Partikeltypen.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'node-terminal-simulator',
@@ -101,9 +101,9 @@ export const projects: Project[] = [
         "It's primary use case is to test CLI applications that rely on ANSI escapes to do terminal magic.",
       de:
         'node-terminal-simulator simuliert das Verhalten des Terminals, einschließlich vieler ANSI Escape-Codes. ' +
-        'Das primäre Anwendungsgebiet ist das Testen von CLI-Anwendungen, die auf ANSI Escapes angewiesen sind, um Magie im Terminal zu machen.'
+        'Das primäre Anwendungsgebiet ist das Testen von CLI-Anwendungen, die auf ANSI Escapes angewiesen sind, um Magie im Terminal zu machen.',
     },
-    gitHubUser: 'LoaderB0T'
+    gitHubUser: 'LoaderB0T',
   },
   {
     name: 'Bl3-EchoCom',
@@ -113,8 +113,8 @@ export const projects: Project[] = [
         'You can find the STL files for 3D printing, the KiCad project for the custom PCB and some example code.',
       de:
         'Dieses Repo enthält alle benötigten Ressourcen, um Ihr eigenes Echo Device aus dem Borderlands-Franchise zu 3D drucken und zu bauen. ' +
-        'Sie finden die STL-Dateien zum 3D-Drucken, das KiCad-Projekt für das PCB und etwas Beispielcode.'
+        'Sie finden die STL-Dateien zum 3D-Drucken, das KiCad-Projekt für das PCB und etwas Beispielcode.',
     },
-    gitHubUser: 'LoaderB0T'
-  }
+    gitHubUser: 'LoaderB0T',
+  },
 ];
