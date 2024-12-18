2a610a2cc3701f2b418f440e0aa27a754e7b2fc7
Janik Schumacher <rndmb0t@gmail.com>
2024-12-18 15:18:24 +0100

    .

--- web/apps/host/src/app/app.config.ts
+++ web/apps/host/src/app/app.config.ts
@@ -8,6 +8,7 @@ import {
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { MissingTranslationHandler, provideTranslateService } from '@ngx-translate/core';
+import { ɵinitializeEnvironment } from 'ng-dynamic-mf/environment';
 
 import { ThemeService, TranslationService } from '@awdware/shared';
 
@@ -25,9 +26,11 @@ export const appConfig: ApplicationConfig = {
         useClass: MyMissingTranslationHandler,
       },
     }),
-    provideAppInitializer(() => {
+    provideAppInitializer(async () => {
       inject(ThemeService).init();
       inject(TranslationService).init();
+      const env = await fetch('environment.json').then(r => r.json());
+      ɵinitializeEnvironment(env);
     }),
     provideHttpClient(),
   ],
--- web/apps/host/src/main.ts
+++ web/apps/host/src/main.ts
@@ -1,11 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { ɵinitializeEnvironment } from 'ng-dynamic-mf/environment';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-fetch('environment.json').then(async response => {
-  const env = await response.json();
-  ɵinitializeEnvironment(env);
-  bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
-});
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
