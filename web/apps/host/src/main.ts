428818ec545b04ceb496a72a225286fd5b8510fb
Janik Schumacher <rndmb0t@gmail.com>
2024-12-18 15:15:42 +0100

    .

--- web/apps/host/src/main.ts
+++ web/apps/host/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { ɵinitializeEnvironment } from 'ng-dynamic-mf/environment';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+fetch('environment.json').then(async response => {
+  const env = await response.json();
+  ɵinitializeEnvironment(env);
+  bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+});
