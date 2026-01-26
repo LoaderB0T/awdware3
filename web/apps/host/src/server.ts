3998b1c9f55c2cece22657f36a1a650f50603ad4
Janik Schumacher <rndmb0t@gmail.com>
2026-01-26 11:34:20 +0100

    fix

--- web/apps/host/src/server.ts
+++ web/apps/host/src/server.ts
@@ -120,7 +120,7 @@ app.use(
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.use('/**', (req, res, next) => {
+app.use((req, res, next) => {
   angularApp
     .handle(req)
     .then(response => (response ? writeResponseToNodeResponse(response, res) : next()))
